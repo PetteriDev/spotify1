5d84cf84ba2e50d295e6e68669f470e37fbec72d
Your Name <you@example.com>
2023-08-30 18:19:59 +0300

    1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const config = require('./config/database');
 const { exec } = require('child_process');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
