bb6229b07a713951a341d3abad8cbe05849f7da4
Your Name <you@example.com>
2023-08-30 18:21:06 +0300

    1

--- search.py
+++ search.py
@@ -36,5 +36,5 @@ def search_artist():
 
 if __name__ == '__main__':
     # Use the PORT environment variable provided by Heroku
-    port = int(os.environ.get(5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
